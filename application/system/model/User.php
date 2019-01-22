3a9e8145c6e745fdfb8a6ae8b6407f027bce67b1
york <910300685@qq.com>
2019-01-22 22:50:56 +0800

    git commit -a -m "version 1.01"

--- application/system/model/User.php
+++ application/system/model/User.php
@@ -5,12 +5,12 @@ namespace app\system\model;
 use think\facade\Request;
 use think\Model;
 
-//用户数据控制器
+//用户数据模型
 class User extends Common {
 
 
     public function getUserInfo(){
         $userId = Request::userId;
-
+        return User::get($userId);
     }
 }
